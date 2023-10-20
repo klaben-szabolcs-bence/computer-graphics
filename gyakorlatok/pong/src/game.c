7eae46db649a1b413b69c41ad45f4152bd9c8757
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:40:16 +0200

    Pong: Make paddles start at the proper center

--- gyakorlatok/pong/src/game.c
+++ gyakorlatok/pong/src/game.c
@@ -17,22 +17,24 @@ void restart_game(Game* game)
     center_x = game->width / 2;
     center_y = game->height / 2;
 
-    move_pad(&(game->left_pad), center_y, game->height);
-    move_pad(&(game->right_pad), center_y, game->height);
+    move_pad(&(game->left_pad), center_y - game->left_pad.size/2, game->height);
+    move_pad(&(game->right_pad), center_y - game->right_pad.size/2, game->height);
     start_ball(&(game->ball), center_x, center_y);
 }
 
 void update_game(Game* game, double time)
 {
-#define BALL_COLLIDE_WITH_LPADLE game->left_pad.position  < game->ball.y && game->left_pad.position + game->left_pad.size > game->ball.y
-#define BALL_COLLIDE_WITH_RPADLE game->right_pad.position < game->ball.y && game->right_pad.position + game->right_pad.size > game->ball.y
+    #define BALL_YCOLLIDE_WITH_LPADLE game->left_pad.position  < game->ball.y && game->left_pad.position + game->left_pad.size > game->ball.y
+    #define BALL_YCOLLIDE_WITH_RPADLE game->right_pad.position < game->ball.y && game->right_pad.position + game->right_pad.size > game->ball.y
+    #define BALL_XCOLLIDE_WITH_LPADLE game->ball.x - game->ball.radius < PAD_WIDTH && game->ball.x + game->ball.radius > 0
+    #define BALL_XCOLLIDE_WITH_RPADLE game->ball.x + game->ball.radius > game->width - PAD_WIDTH && game->ball.x - game->ball.radius < game->width
 
     update_ball(&(game->ball), time);
-    if (game->ball.x - game->ball.radius < 50 && BALL_COLLIDE_WITH_LPADLE) {
+    if (BALL_XCOLLIDE_WITH_LPADLE && BALL_YCOLLIDE_WITH_LPADLE) {
         game->ball.x = game->ball.radius + 50;
         game->ball.speed_x *= -1.1;
     }
-    if (game->ball.x + game->ball.radius > game->width - 50 && BALL_COLLIDE_WITH_RPADLE) {
+    if (BALL_XCOLLIDE_WITH_RPADLE && BALL_YCOLLIDE_WITH_RPADLE) {
         game->ball.x = game->width - game->ball.radius - 50;
         game->ball.speed_x *= -1.1;
     }
