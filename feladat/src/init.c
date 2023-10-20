6ed14b93049c40966fdb453490e3aec7ae1399ab
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:40:35 +0200

    Enable transparency

--- feladat/src/init.c
+++ feladat/src/init.c
@@ -21,6 +21,8 @@ void init_opengl()
     glClearDepth(1.0);
 
     glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
